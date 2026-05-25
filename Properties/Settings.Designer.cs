99702361c2d8d4885980fb0161d65ab1eaa548bd
Thomas <0870156@schoolsnet.act.edu.au>
2026-05-25 18:28:58 +1000

    pushing all files.

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace IT_Assesment_Start.Properties
+namespace IT_Assesment_Start.Properties 
 {
 
 
